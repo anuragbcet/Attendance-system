049a76be658c0f94d4be9f2c8becdf59a0768367
Anurag Kumar <anuragbcet2023@gmail.com>
2026-01-31 18:20:15 +0530

    signup and signin route complted

--- routers/user.ts
+++ routers/user.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { signUpSchema } from "../utils/types";
+import { signInSchema, signUpSchema } from "../utils/types";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from "../models/user.model";
+
+const JWT_SECRET = process.env.JWT_SECRET || "";
 
 const userRouter = Router();
 
@@ -29,11 +33,14 @@ userRouter.post("/auth/signup", async (req, res) => {
         error: "Email already exists",
       });
     }
+    
+    const hashedPassword = await bcrypt.hash(password,10);
+    console.group(hashedPassword);
     const newUser = await User.create({
       name: name,
       email: email,
-      password: password,
-      role: role,
+      password: hashedPassword,
+      role: "Student",
     });
     return res.status(201).json({
       success: true,
@@ -52,6 +59,43 @@ userRouter.post("/auth/signup", async (req, res) => {
   }
 });
 
-
+userRouter.post("/auth/signin", async (req, res) => {
+  const result = signInSchema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json({
+      success: false,
+      error: "Invalid request schema",
+    });
+  }
+  try {
+    const {email,password} = result.data;
+    const user = await User.findOne({
+      email
+    });
+    if(!user){
+      return res.status(400).json({
+        success: false,
+        error: "User not found",
+      });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if(match){
+      const token = jwt.sign({
+        userId: user._id,
+        role: user.role
+      }, JWT_SECRET, { expiresIn: '1d' })
+      return res.status(201).json({
+        status:"success",
+        token:token
+      })
+    }
+    return res.status(401).json({
+      status:"fail",
+      message:"Enter correct password"
+    })
+  } catch (error) {
+    return res.status(401).json({message:"User not authorized"})
+  }
+});
 
 export default userRouter;
